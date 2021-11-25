48b73788068e9946a254a34b70e5b20095dc6d29
Shane Nicholson <shanescottnicholson@gmail.com>
2021-11-24 16:18:33 -0800

    Adds rotating square using two.js

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1></h1>
+  </div>
+</template>
+
+<script>
+import Two from "two.js";
+
+var two = new Two({
+  fullscreen: true,
+  autostart: true,
+}).appendTo(document.body);
+var rect = two.makeRectangle(two.width / 2, two.height / 2, 50, 50);
+two.bind("update", function () {
+  rect.rotation += 0.1;
+});
+
+console.log("Importing this square using their docs took me nearly an hour and a half.");
+</script>
